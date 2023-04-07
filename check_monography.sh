bccf78a78f1b71d2d7fce8848328f1a2a57fded4
SHtress <stas.butrin1331@gmail.com>
2023-04-07 18:17:24 +0300

    feat: add bash script for checking full monography

---
+++ check_monography.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+find ../ -type f -name "chapter_*.tex" -print0 | while read -d $'\0' file
+do
+    ./check_term_biblio.py $file -g ../author/glossary.tex -b ../bibliography.tex
+done
+
+#./check_term_biblio.py $1 -g ../author/glossary.tex -b ../bibliography.tex
